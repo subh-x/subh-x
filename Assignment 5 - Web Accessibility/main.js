377e7c2a3cd741042284a483dc3b12f093772f9c
Subham Thakur <subham.thakur@dcmail.ca>
2025-03-31 15:41:50 +0530

    Update main.js
    updated the javascript code and some other needy elements has been added.

--- Assignment 5 - Web Accessibility/main.js
+++ Assignment 5 - Web Accessibility/main.js
@@ -4,7 +4,7 @@
 	Date: 31 March 2025
 Address problems with color contrast, semantic HTML, photos, music, forms, interactive components, and tables to make a fictitious nature site more accessible. Test and modify the background and text colors to improve readability. For improved screen reader navigation, update the HTML structure by using a semantic <nav> element for <div class="nav">. To accommodate deaf people and give a fallback for outdated browsers, make sure pictures include alt attributes and provide transcripts for the <audio> element. By appropriately linking <label> components with inputs and providing screen reader-only labels, you may increase form accessibility. Make sure users can concentrate and use the keyboard to trigger the show/hide comments feature. Include a summary for clarity and header associations to increase table accessibility. As a result, the website is completely accessible and inclusive to all users.
 **/
-  // functionality for showing/hiding the comments section
+// functionality for showing/hiding the comments section
 
 const showHideBtn = document.querySelector('.show-hide');
 const commentWrapper = document.querySelector('.comment-wrapper');
@@ -21,9 +21,11 @@ showHideBtn.onclick = function() {
     commentWrapper.style.display = 'none';
   }
 };
-
-// functionality for adding a new comment via the comments form
-
+showHideBtn.onkeydown = (e) => {
+  if (e.key === "Enter") {
+    showHideBtn.click();
+  }
+}
 const form = document.querySelector('.comment-form');
 const nameField = document.querySelector('#name');
 const commentField = document.querySelector('#comment');
@@ -51,3 +53,33 @@ function submitComment() {
   nameField.value = '';
   commentField.value = '';
 }
+
+const transcript = document.querySelector('.transcript');
+const transcriptBtn = document.querySelector('.transcript-button');
+const transcriptCon = document.querySelector('.transcript-container')
+
+transcriptBtn.onclick = () => {
+  if(transcriptBtn.textContent === 'Show transcript') {
+    transcript.style.display = "block";
+    transcriptBtn.textContent = 'Hide transcript';
+  } else {
+    transcript.style.display = "none";
+    transcriptBtn.textContent = 'Show transcript'
+  }
+}
+
+const nav = document.querySelector("nav")
+const hamIcon = document.querySelector(".hamIcon")
+const navMenu = document.querySelector("nav ul")
+navMenu.className = "hidden";
+
+hamIcon.onclick = () => {
+  if(navMenu.className === "hidden") {
+    navMenu.style.visibility = "visible";
+    navMenu.className = "visible";
+  } else if (navMenu.className === "visible") {
+    navMenu.style.visibility = "hidden";
+    navMenu.className = "hidden";
+  }
+}
+
